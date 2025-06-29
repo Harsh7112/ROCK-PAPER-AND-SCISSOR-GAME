622f829f5e42a630f2404e508458d4200eed6094
Harsh Tiwari <harsh.tiwari7112@gmail.com>
2025-06-29 10:37:41 +0530

    Game.py

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import random
 
 options = ("rock" ,"paper" ,"scissor")
 
+name = input("Enter your name: ")
 player = None
 computer = random.choice(options)
 player = input("Enter your choice[rock,paper,scissor]: ")
@@ -9,18 +10,20 @@ print(f"player: {player}")
 print(f"computer: {computer}")
 
 if(player == "rock" and computer == "scissor"):
-print("PLAYER WINS")
+    print("PLAYER WINS")
 elif(player == "scissor" and computer == "paper"):
-print("PLAYER WINS")
+    print("PLAYER WINS")
 elif(player == "paper" and computer == "rock"):
-print("PLAYER WINS")
+    print("PLAYER WINS")
 elif(player == "rock" and computer == "paper"):
-print("COMPUTER WINS")
+    print("COMPUTER WINS")
 elif(player == "scissor" and computer == "rock"):
-print("COMPUTER WINS")
+    print("COMPUTER WINS")
 elif(player == "paper" and computer == "scissor"):
-print("COMPUTER WINS")
+    print("COMPUTER WINS")
 elif(player == computer):
-print("TIE")
+    print("TIE")
 else:
-("INVALID INPUT BY PLAYER")
+    print("INVALID INPUT BY PLAYER")
+
+print(f"THANKS FOR PLAYING {name}")
