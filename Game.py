5e8538be1555ca13d0409a826a4535cc2d992f77
Harsh Tiwari <harsh.tiwari7112@gmail.com>
2025-07-12 15:35:34 +0530

    Game.py
    Now I added a while function for continue playing but I didn't add stop function for it but I'II do it earlier

--- Game.py
+++ Game.py
@@ -1,29 +1,33 @@
 import random
 
 options = ("rock" ,"paper" ,"scissor")
-
 name = input("Enter your name: ")
 player = None
-computer = random.choice(options)
-player = input("Enter your choice[rock,paper,scissor]: ")
-print(f"player: {player}")
-print(f"computer: {computer}")
 
-if(player == "rock" and computer == "scissor"):
-    print("PLAYER WINS")
-elif(player == "scissor" and computer == "paper"):
-    print("PLAYER WINS")
-elif(player == "paper" and computer == "rock"):
-    print("PLAYER WINS")
-elif(player == "rock" and computer == "paper"):
-    print("COMPUTER WINS")
-elif(player == "scissor" and computer == "rock"):
-    print("COMPUTER WINS")
-elif(player == "paper" and computer == "scissor"):
-    print("COMPUTER WINS")
-elif(player == computer):
-    print("TIE")
-else:
-    print("INVALID INPUT BY PLAYER")
+is_running = True
 
-print(f"THANKS FOR PLAYING {name}")
+while True:
+    computer = random.choice(options)
+    player = input("Enter your choice[rock,paper,scissor]: ")
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    if(player == "rock" and computer == "scissor"):
+        print("PLAYER WINS")
+    elif(player == "scissor" and computer == "paper"):
+        print("PLAYER WINS")
+    elif(player == "paper" and computer == "rock"):
+        print("PLAYER WINS")
+    elif(player == "rock" and computer == "paper"):
+        print("COMPUTER WINS")
+    elif(player == "scissor" and computer == "rock"):
+        print("COMPUTER WINS")
+    elif(player == "paper" and computer == "scissor"):
+        print("COMPUTER WINS")
+    elif(player == computer):
+        print("TIE")
+    else:
+        print("INVALID INPUT BY PLAYER")
+    
+    print(f"THANKS FOR PLAYING {name}")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
